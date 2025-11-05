9ff20c8e46b8f26c451cdc3b1039d29e9d440681
Roopesh O R <62459820+roopeshor@users.noreply.github.com>
2025-11-05 18:30:44 +0530

    fix variable name

--- krotate-right.sh
+++ krotate-right.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-STATE_F="$HOME/.config/krotate.rc"
+STATE_FILE="$HOME/.config/krotate.rc"
 
 
-[[ ! -f "$STATE_F" ]] && echo "n" > "$STATE_F"
+[[ ! -f "$STATE_FILE" ]] && echo "n" > "$STATE_FILE"
 
-state=$(cat "$STATE_F")
+state=$(cat "$STATE_FILE")
 OUTPUT_DISP=$(kscreen-doctor -o | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" | grep -oP '(?<=Output: 1 ).*(?=\s)')
 
 if [[ "$state" == "n" ]]; then
-	echo "r" > "$STATE_F"
+	echo "r" > "$STATE_FILE"
 	kscreen-doctor output."$OUTPUT_DISP".rotation.right
 elif [[ "$state" == "r" ]]; then
-	echo "i" > "$STATE_F"
+	echo "i" > "$STATE_FILE"
         kscreen-doctor output."$OUTPUT_DISP".rotation.inverted
 elif [[ "$state" == "i" ]]; then
-	echo "l" > "$STATE_F"
+	echo "l" > "$STATE_FILE"
         kscreen-doctor output."$OUTPUT_DISP".rotation.left
 else
-	echo "n" > "$STATE_F"
+	echo "n" > "$STATE_FILE"
         kscreen-doctor output."$OUTPUT_DISP".rotation.normal
 fi
